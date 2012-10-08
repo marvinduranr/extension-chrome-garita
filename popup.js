2facabd97fc3208e44924a7e6c14c436bf5a5348
volocake <juanitopasten@gmail.com>
2012-10-08 16:47:10 -0700

    Se modificaron los estilos, el manifesto (nombre 'real'), y ya manda bien la informacion el JavaScript

--- popup.js
+++ popup.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-//http://apps.cbp.gov/bwt/customize_rss.asp?portList=250601,250401,250501&lane=all&action=rss&f=html
-//http://apps.cbp.gov/bwt/customize_rss.asp?portList=250601,250401&lane=all&action=rss&f=html
+//RSS SanYsidro-Otay-TKT http://apps.cbp.gov/bwt/customize_rss.asp?portList=250601,250401,250501&lane=all&action=rss&f=html
+//RSS SanYsidro-Otay http://apps.cbp.gov/bwt/customize_rss.asp?portList=250601,250401&lane=all&action=rss&f=html
   $.ajax({
     type: 'POST',
-    url: 'http://apps.cbp.gov/bwt/customize_rss.asp?portList=250601,250401,250501&lane=all&action=rss&f=html',
+    url: 'http://apps.cbp.gov/bwt/customize_rss.asp?portList=250601,250401&lane=all&action=rss&f=html',
     cache: false,  
 
     beforeSend:function(){
@@ -15,9 +15,7 @@ $(document).ready(function() {
       $('#div_ajax').empty();
       $('#estado').empty();
       $(data).find('item').each(function(i){
-      $('#div_ajax').append('<p>' + $(this).find('title').text() + '</p>');
-      //$('#div_ajax').append('<p>' + $(this).find('description').text() + '</p>');
-      $('#div_ajax').append('<p>'+tiempo($(this).find('description').text())+'</p>');
+      $('#div_ajax').append('<p>'+tiempo($(this).find('title').text() , $(this).text())+'</p>');
       });
     },
     error:function(){
@@ -26,25 +24,38 @@ $(document).ready(function() {
     }
   });
 
-  function tiempo(texto){
-      
-        var indice=texto.indexOf('PDT');
-        var garita = "Standard "+ texto.slice(indice + 3);
-        var modificado = garita;
-        var fin = garita.indexOf("lane(s)");
-        garita=garita.slice(0,fin - 3);
-        modificado = modificado.slice(fin);
+  function tiempo(garita, info){
+      //tomo nombre de la garita
+      var garita_info= garita.slice(0,garita.indexOf('-')) + "<br/>";
 
-        indice=modificado.indexOf('PDT');
-        garita=garita+ "ReadyLine "+modificado.slice(indice+3);
-        modificado=garita;
-        fin=garita.indexOf("lane(s)");
-        garita=garita.slice(0,fin-3);
-        modificado=modificado.slice(fin);
-      
-        return garita
+      //posicion y corto la informacion para comenzar a tomar el tiempo
+      var passenger_vehicles= info.indexOf('Passenger Vehicles');
+      var info_modificado=info.slice(passenger_vehicles);
+      var pos_inicio;
+      var pos_fin;
 
-        
+      for(i=0;i<4;i++){
+        switch(i){
+          case 0: garita_info = garita_info + "Standard ";
+                  break;
+          case 1: garita_info = garita_info + "Readylane ";
+                  break;
+          case 2: garita_info = garita_info + "Sentri ";
+                  break;
+          case 3: garita_info = garita_info + "Peatonal ";
+                  break;
+        }
+	//tomo posicion del tiempo y lo agrego.
+        pos_inicio=info_modificado.indexOf('PDT');
+        pos_fin = info_modificado.indexOf('lane(s)');
+        garita_info = garita_info + info_modificado.slice(pos_inicio+3, pos_fin-3)+"<br/>";
+        info_modificado=info_modificado.slice(pos_fin+4);
+
+      }
+      garita_info=garita_info.replace(",",'');
+      garita_info = garita_info +"<br/>"
+      return garita_info
+
   }
 });
 
