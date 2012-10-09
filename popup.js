e6c40524be6231b0da97fe1bdcb20e58ff8d49a0
volocake <juanitopasten@gmail.com>
2012-10-09 09:26:30 -0700

    Se agrego estilos a la informacion y se muestra en formato tabla

--- popup.js
+++ popup.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 //RSS SanYsidro-Otay-TKT http://apps.cbp.gov/bwt/customize_rss.asp?portList=250601,250401,250501&lane=all&action=rss&f=html
 //RSS SanYsidro-Otay http://apps.cbp.gov/bwt/customize_rss.asp?portList=250601,250401&lane=all&action=rss&f=html
+//RSS Clima Tijuana http://rss.weather.com/weather/rss/local/MXBC0005?cm_ven=LWO&cm_cat=rss&par=LWO_rss
   $.ajax({
     type: 'POST',
     url: 'http://apps.cbp.gov/bwt/customize_rss.asp?portList=250601,250401&lane=all&action=rss&f=html',
@@ -15,7 +16,7 @@ $(document).ready(function() {
       $('#div_ajax').empty();
       $('#estado').empty();
       $(data).find('item').each(function(i){
-      $('#div_ajax').append('<p>'+tiempo($(this).find('title').text() , $(this).text())+'</p>');
+      $('#div_ajax').append(tiempo($(this).find('title').text() , $(this).text()));
       });
     },
     error:function(){
@@ -26,7 +27,9 @@ $(document).ready(function() {
 
   function tiempo(garita, info){
       //tomo nombre de la garita
-      var garita_info= garita.slice(0,garita.indexOf('-')) + "<br/>";
+      //var garita_info= "<span class='titulo'>"+garita.slice(0,garita.indexOf('-')) + "</span><br/>";
+
+      var garita_info="<table class='table table-striped'><thead><tr><th colspan=2 class='titulo'>"+garita.slice(0,garita.indexOf('-'))+"</th></tr></thead><tbody>";
 
       //posicion y corto la informacion para comenzar a tomar el tiempo
       var passenger_vehicles= info.indexOf('Passenger Vehicles');
@@ -36,26 +39,28 @@ $(document).ready(function() {
 
       for(i=0;i<4;i++){
         switch(i){
-          case 0: garita_info = garita_info + "Standard ";
+          case 0: garita_info = garita_info + "<tr><td>Standard</td> ";
                   break;
-          case 1: garita_info = garita_info + "Readylane ";
+          case 1: garita_info = garita_info + "<tr><td>Readylane</td>";
                   break;
-          case 2: garita_info = garita_info + "Sentri ";
+          case 2: garita_info = garita_info + "<tr><td>Sentri</td>";
                   break;
-          case 3: garita_info = garita_info + "Peatonal ";
+          case 3: garita_info = garita_info + "<tr><td>Peatonal</td>";
                   break;
         }
 	//tomo posicion del tiempo y lo agrego.
         pos_inicio=info_modificado.indexOf('PDT');
         pos_fin = info_modificado.indexOf('lane(s)');
-        garita_info = garita_info + info_modificado.slice(pos_inicio+3, pos_fin-3)+"<br/>";
+        garita_info = garita_info + "<td>" +info_modificado.slice(pos_inicio+3, pos_fin-3)+"</td></tr>";
         info_modificado=info_modificado.slice(pos_fin+4);
 
       }
       garita_info=garita_info.replace(",",'');
-      garita_info = garita_info +"<br/>"
+      garita_info = garita_info +"</tbdoy></table>"
       return garita_info
 
   }
+
+
 });
 
